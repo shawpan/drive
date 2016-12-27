1ed6118beaa0e4c6f61f8f3e7238dc7b009995c9
Akiz <shawpan.du@gmail.com>
2016-12-27 21:56:52 +0600

    Best so far at 20km/h

--- model.py
+++ model.py
@@ -7,9 +7,12 @@ import csv
 import pickle
 import cv2
 import numpy as np
+import math
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten, Lambda, ELU
 from keras.layers.convolutional import Convolution2D
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
 
@@ -20,45 +23,48 @@ tf.python.control_flow_ops = tf
 def get_model():
     """ Get the designed NN model
     """
-    ch, row, col = 3, 160, 320
+    ch, row, col = 3, 45, 160
 
     model = Sequential()
     model.add(Lambda(lambda x: x/255. - 0.5,
-            input_shape=(ch, row, col),
-            output_shape=(ch, row, col)))
+            input_shape=(row, col, ch),
+            output_shape=(row, col, ch)))
+    # model.add(Convolution2D(3, 1, 1, border_mode="same"))
+    # model.add(ELU())
     model.add(Convolution2D(16, 8, 8, subsample=(4, 4), border_mode="same"))
     model.add(ELU())
     model.add(Convolution2D(32, 5, 5, subsample=(2, 2), border_mode="same"))
     model.add(ELU())
     model.add(Convolution2D(64, 5, 5, subsample=(2, 2), border_mode="same"))
     model.add(Flatten())
-    model.add(Dropout(.2))
+    model.add(Dropout(.5))
     model.add(ELU())
     model.add(Dense(512))
     model.add(Dropout(.5))
     model.add(ELU())
     model.add(Dense(1))
 
-    model.compile(optimizer="adam", loss="mse")
-
     return model
 
-
-def data_generator (x, y, batch_size):
-    """ Generates (inputs, outputs)
+def train_data_generator (x, y, batch_size):
+    """ Generates (inputs, outputs) for training in batches
     """
+    total = (len(y) // batch_size ) * batch_size
     while True:
-        start = np.random.randint(low = 0, high = len(y) - batch_size)
-        x_data = np.empty((0,3,160,320))
-        y_data = y[start:start + batch_size,:]
+        start = np.random.randint(0, total - batch_size)
+        end = min(start + batch_size, total)
+        x_data = np.empty((0,45,160, 3))
+        y_data = np.array([])
 
-        index = start
-        for i in np.arange(batch_size):
-            image = cv2.imread('./data/' + x[index][0])
-            image_h, image_w = image.shape[0], image.shape[1]
-            image = image.reshape(3, image_h, image_w)
+        for i in np.arange(start, end):
+            image = cv2.imread('./data/' + x[i][0])
+            image = image[40:130]
+            image = cv2.resize(image, (0,0), fx=0.5, fy=0.5)
+            flipped_image = cv2.flip(image, 1)
             x_data = np.append(x_data, np.array([image]), axis = 0)
-            index += 1
+            y_data = np.append(y_data, y[i])
+            x_data = np.append(x_data, np.array([flipped_image]), axis = 0)
+            y_data = np.append(y_data, -1 * y[i])
         yield x_data, y_data
 
 def train():
@@ -68,14 +74,24 @@ def train():
         data = pickle.load(data_file)
 
     x_train, y_train, x_val, y_val = data['x_train'], data['y_train'], data['x_val'], data['y_val']
+    print("Train data : (X, Y) ",  x_train.shape, y_train.shape)
+    print("Validation data : (X, Y) ",  x_val.shape, y_val.shape)
+
+    nb_epoch = 1
+    batch_size = 16
     model = get_model()
-    model.fit_generator(
-        data_generator(x_train, y_train, 10),
-        samples_per_epoch = 10,
-        nb_epoch = 2,
-        verbose = 2,
-        validation_data = data_generator(x_val, y_val,10),
-        nb_val_samples = 10
+    adam = Adam(lr=1e-6)
+    model.compile(optimizer=adam, loss="mse")
+    model.load_weights('model_best3.h5')
+    model.summary()
+    #checkpointer = ModelCheckpoint(filepath="model.h5", verbose=1, save_best_only=True)
+    history = model.fit_generator(
+        train_data_generator(x_train, y_train, batch_size),
+        samples_per_epoch = ((len(y_train) // batch_size ) * batch_size) * 2,
+        nb_epoch = nb_epoch,
+        verbose = 1,
+        validation_data = train_data_generator(x_val, y_val, batch_size),
+        nb_val_samples = ((len(y_val) // batch_size ) * batch_size) * 2
     )
     # Save weights
     model.save_weights("./model.h5", True)
@@ -91,29 +107,25 @@ def build_data():
         reader = csv.DictReader(log_file)
         x , y = extra_x = [], []
         for row in reader:
-            x.append(row['center'])
-            y.append(row['steering'])
+            speed = float(row['speed'])
+            steering_angle = round(float(row['steering']),3)
+            x.append(row['left'].strip())
+            y.append(steering_angle + 0.25)
+            x.append(row['center'].strip())
+            y.append(steering_angle)
+            x.append(row['right'].strip())
+            y.append(steering_angle - 0.25)
         x = np.vstack(x)
         y = np.vstack(y)
-
-    # Shuffle data
-    # shuffle(x,y, random_state = 27672)
     print("Total data : (X, Y) ",  x.shape, y.shape)
     # Split into train and test data
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=3234)
-    print("Train data : (X, Y) ",  x_train.shape, y_train.shape)
-    print("Test data : (X, Y) ",  x_test.shape, y_test.shape)
-    # Split train data into train and validation data
-    x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.33, random_state=937)
-    print("Train data : (X, Y) ",  x_train.shape, y_train.shape)
-    print("Validation data : (X, Y) ",  x_val.shape, y_val.shape)
+    x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.15, random_state=3234)
+
     data = {
         'x_train' : x_train,
         'y_train' : y_train,
         'x_val' : x_val,
-        'y_val' : y_val,
-        'x_test' : x_test,
-        'y_test' : y_test
+        'y_val' : y_val
     }
     # Save as a file
     with open('data.p', "wb") as data_file:
@@ -122,10 +134,7 @@ def build_data():
 
 if __name__ == '__main__':
     # Build train, validationa nd test data
-    if not os.path.isfile('data.p'):
-        print("Constructing training, validation and test data from log file")
-        build_data()
-    else:
-        print("Training, validation and test data exist already")
+    #build_data()
     # Start training
+    print("Training started")
     train()
